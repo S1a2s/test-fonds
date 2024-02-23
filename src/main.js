b4f6935df061129ee5aa6ab9c8aa379942587d38
S1a2s <konov.a.9911@gmail.com>
2024-02-23 13:08:01 +0200

    .

--- src/main.js
+++ src/main.js
@@ -1,6 +1,35 @@
-var swiper = new Swiper(".mySwiper", {
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
+// import Swiper from 'swiper';
+
+
+// const swiper = new Swiper('.my-swiper', {
+//   direction: 'vertical',
+//   spaceBetween: 20,
+//   slidesPerView: 'auto',
+
+//   navigation: {
+//     nextEl: '.swiper-button-down',
+//     prevEl: '.swiper-button-top',
+//   },
+
+//   plugins: {
+//     scrollContainer: true,
+//   },
+// });
+ const hiddenItems = document.querySelectorAll('.fonds-item--hidden');
+    const toggleButton = document.getElementById('toggleButton');
+    
+    // Функція, яка показує або ховає елементи списку в залежності від їх стану
+    function toggleItems() {
+      hiddenItems.forEach(item => {
+        item.classList.toggle('fonds-item--hidden');
+      });
+      // Зміна тексту кнопки
+      if (toggleButton.innerText === 'Показати ще') {
+        toggleButton.innerText = 'Показати менше';
+      } else {
+        toggleButton.innerText = 'Показати ще';
+      }
+    }
+    
+    // Додаємо обробник кліку на кнопку
+    toggleButton.addEventListener('click', toggleItems);
